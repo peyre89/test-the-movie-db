de1f5f9eae0b0f51d85fd040001dd3a77f9773cc
Florian Peyre <peyre89@gmail.com>
2021-10-09 12:18:39 +0200

    new dir api + new Axios instance + first API method getMovieUpcoming

---
+++ src/api/index.ts
@@ -0,0 +1,14 @@
+import axios, { AxiosInstance } from 'axios';
+
+const API_KEY = process.env.REACT_APP_API_KEY;
+const API_URL = 'https://api.themoviedb.org/3';
+
+const instance: AxiosInstance = axios.create({
+  baseURL: API_URL,
+  params: {
+    api_key: API_KEY,
+    language: 'en-US',
+  },
+});
+
+export const getMovieUpcoming = () => instance.get('/movie/upcoming');
