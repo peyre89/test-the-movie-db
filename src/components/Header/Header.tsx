fd20f91c7c6bf64c2139f2296a11dfe307978bcb
Florian Peyre <peyre89@gmail.com>
2021-10-09 20:54:59 +0200

    Display of the number of movies in My list

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,16 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { SliceState } from 'features/list/listSlice';
+import { RootState } from 'store';
 
 import './Header.scss';
 
 function Header() {
+  const list: SliceState = useSelector((state: RootState) => state.list.movies);
+
+  const labelMyList = list.length > 0 ? `My list (${list.length})` : 'My list';
+
   return (
     <header className="Header">
       <nav>
@@ -11,7 +19,7 @@ function Header() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/my-list">My list</Link>
+            <Link to="/my-list">{labelMyList}</Link>
           </li>
           <li>
             <Link to="/about">About</Link>
