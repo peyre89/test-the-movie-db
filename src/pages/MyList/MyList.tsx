fd95c1c6196cdcb47babd25ed2469182b1a3162a
Florian Peyre <peyre89@gmail.com>
2021-10-09 20:46:43 +0200

    Display of the movies on the My list page

--- src/pages/MyList/MyList.tsx
+++ src/pages/MyList/MyList.tsx
@@ -1,11 +1,28 @@
+import { useSelector } from 'react-redux';
+
+import { SliceState } from 'features/list/listSlice';
+import { RootState } from 'store';
+import { Movie } from 'types';
+
 import Layout from 'components/Layout';
+import MovieCard from 'components/MovieCard';
 
 import './MyList.scss';
 
 function MyList() {
+  const list: SliceState = useSelector((state: RootState) => state.list.movies);
+
   return (
     <Layout>
-      <div className="MyList">My list</div>
+      <div className="MyList">
+        <ul>
+          {list.map((movie: Movie) => (
+            <li key={movie.id}>
+              <MovieCard movie={movie} />
+            </li>
+          ))}
+        </ul>
+      </div>
     </Layout>
   );
 }
