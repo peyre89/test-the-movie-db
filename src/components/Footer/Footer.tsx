e8f457d7e5494e06b7711eeb5a2dded9e0ef33d7
Florian Peyre <peyre89@gmail.com>
2021-10-10 21:45:12 +0200

    Link to the GitHub repository

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,24 @@
+import IconButton from '@mui/material/IconButton';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
 import './Footer.scss';
 
 function Footer() {
-  return <footer className="Footer">Footer...</footer>;
+  return (
+    <footer className="Footer">
+      <IconButton
+        component="a"
+        href="https://github.com/peyre89/test-the-movie-db"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="GitHub"
+        size="large"
+        sx={{ color: '#000' }}
+      >
+        <GitHubIcon fontSize="inherit" />
+      </IconButton>
+    </footer>
+  );
 }
 
 export default Footer;
