fc8d307411d988523239187c548330cde4aac2e7
Florian Peyre <peyre89@gmail.com>
2021-10-10 23:03:19 +0200

    Fix App Test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/hello world/i);
-  expect(linkElement).toBeInTheDocument();
+test('Test App', () => {
+  const { container } = render(<App />);
+
+  const app = container.firstChild;
+  expect(app).toBeTruthy();
 });
