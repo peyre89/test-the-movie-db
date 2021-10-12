6ea9134d2d773e1074bcdbc5dfeeecaaeff869a5
Florian Peyre <peyre89@gmail.com>
2021-10-12 20:35:56 +0200

    interface update for Movie

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,17 +2,61 @@ export interface ApiResults {
   results: [];
 }
 
+///
+
 export type Genre = {
   id: number;
   name: string;
 };
 
-export interface Movie {
+export type ProductionCompany = {
   id: number;
-  title: string;
-  poster_path: string;
-  release_date: string;
-  runtime: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+};
+
+export type ProductionCountry = {
+  iso_3166_1: string;
+  name: string;
+};
+
+export type SpokenLanguage = {
+  english_name: string;
+  iso_639_1: string;
+  name: string;
+};
+
+export interface Movie {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: {
+    backdrop_path: string;
+    id: number;
+    name: string;
+    poster_path: string;
+  };
+  budget: number;
+  genre_ids: number[];
   genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
   overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompany[];
+  production_countries: ProductionCountry[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguage[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
 }
