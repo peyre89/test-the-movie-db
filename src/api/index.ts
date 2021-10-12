1869ff665f080a6a8235141f4da7ecd86b8d0596
Florian Peyre <peyre89@gmail.com>
2021-10-12 20:51:32 +0200

    File API refacto

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,21 +1,18 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
-const API_KEY = process.env.REACT_APP_API_KEY;
-const API_URL = 'https://api.themoviedb.org/3';
-
 const instance: AxiosInstance = axios.create({
-  baseURL: API_URL,
+  baseURL: 'https://api.themoviedb.org/3',
   params: {
-    api_key: API_KEY,
+    api_key: process.env.REACT_APP_API_KEY,
     language: 'en-US',
   },
 });
 
-// Special method
 export const getConfiguration = (): Promise<AxiosResponse> =>
   instance.get('/configuration');
 
 export const getMovieUpcoming = (): Promise<AxiosResponse> =>
   instance.get('/movie/upcoming');
+
 export const getMovieById = (id: string): Promise<AxiosResponse> =>
   instance.get(`/movie/${id}`);
