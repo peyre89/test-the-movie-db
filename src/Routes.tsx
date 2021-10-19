45a94c80aee483cb783c983c698c0b2325f86e35
Florian Peyre <peyre89@gmail.com>
2021-10-19 21:00:01 +0200

    BrowserRouter to HashRouter

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 const About = lazy(() => import('./pages/About'));
 const Movie = lazy(() => import('./pages/Movie'));
